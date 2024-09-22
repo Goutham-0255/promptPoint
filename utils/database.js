f090f420445c15d28a11d42a16924ef2e4a76663
Goutham-0255 <gouthamf01@gmail.com>
2024-09-23 03:13:31 +0530

    created a post page

--- utils/database.js
+++ utils/database.js
@@ -15,8 +15,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI,{
             dbName: "share_prompt",
-            useNewParser: true,
-            useUnifiedTopology: true,
         })
         
         isConnected = true;
